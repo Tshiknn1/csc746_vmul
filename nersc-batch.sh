958fa17e0e93b83f819d68dd9bfecf4518551e13
student <tshiknn@gmail.com>
2025-10-03 15:57:36 -0700

    Add batch script

---
+++ nersc-batch.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -C cpu
+#SBATCH -q regular
+#SBATCH -J csc746_f25_evan_caplinger_sum
+#SBATCH --mail-user=ecaplinger@sfsu.edu
+#SBATCH --mail-type=ALL
+#SBATCH -t 00:15:00
+
+#OpenMP settings:
+export OMP_PLACES=threads
+export OMP_PROC_BIND=spread
+
+#run the application:
+srun -n 1 -c 1 --cpu_bind=cores build/benchmark-basic
+srun -n 1 -c 1 --cpu_bind=cores build/benchmark-vectorized
+srun -n 1 -c 1 --cpu_bind=cores --export=ALL,OMP_NUM_THREADS=1 build/benchmark-openmp
+srun -n 1 -c 4 --cpu_bind=cores --export=ALL,OMP_NUM_THREADS=4 build/benchmark-openmp
+srun -n 1 -c 16 --cpu_bind=cores --export=ALL,OMP_NUM_THREADS=16 build/benchmark-openmp
+srun -n 1 -c 64 --cpu_bind=cores --export=ALL,OMP_NUM_THREADS=64 build/benchmark-openmp
